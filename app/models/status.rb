f43a75f521dd2555e069a907f783954891e0985f
Venicio <venicioantunes70@msn.com>
2014-08-18 23:50:39 +0000

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to boostrap/simple_form
    Added Login/Logout/Registration links

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#  user_id    :integer
+#
+
 class Status < ActiveRecord::Base
+	belongs_to :user
 end
--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#  user_id    :integer
+#
+
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
